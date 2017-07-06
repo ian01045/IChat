f6899e55210db47c591f42244d52238790d8c3f1
ian0515 <ian0515@gmail.com>
2017-07-06 17:12:31 +0800

    測試認可同步

---
+++ IChat/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IChat.Models;
+
+namespace IChat.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        db_ichatEntities dc = new db_ichatEntities();
+        public ActionResult Index()
+        {
+            return View(dc.user_master);
+        }
+    }
+}
