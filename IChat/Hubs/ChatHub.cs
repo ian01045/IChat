097bcf00f9281aebec4d2aaa4ea08983898902d2
ian0515 <ian0515@gmail.com>
2017-07-11 18:29:54 +0800

    signalr

---
+++ IChat/Hubs/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+
+namespace IChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        //public void Hello()
+        //{
+        //    Clients.All.hello();
+        //}
+
+        public void Send(string who, string message)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            //Clients.All.addNewMessageToPage(name, message);
+            string id_name = Context.User.Identity.Name;
+            Clients.Group(id_name).addChatMessage(id_name, message);
+            //Clients.Client(id_name).addNewMessageToPage(message);
+        }
+        public override Task OnConnected()
+        {
+            string name = Context.User.Identity.Name;
+            Groups.Add(Context.ConnectionId, name);
+            return base.OnConnected();
+        }
+    }
+}
---
+++ IChat/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Linq;
+using IChat;
+using Microsoft.AspNet.SignalR;
+using System.Web.Mvc;
+
+[assembly: OwinStartup(typeof(IChat.Startup))]
+
+namespace IChat
+{
+    public class Startup
+    {
+        //public void Configuration(IAppBuilder app)
+        //{
+        //    // 如需如何設定應用程式的詳細資訊，請參閱  http://go.microsoft.com/fwlink/?LinkID=316888
+        //}
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
